d7cb36db7a07c36464504eeea90f985ca8a2c8b7
Clifton <clifton3833@gmail.com>
2025-01-21 20:57:51 -0800

    Added Icons to Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { color, motion } from 'framer-motion';
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Contact = () => {
+  const handleclickGithub = () =>{
+    window.open("https://github.com/Cliftan", "_blank");
+  }
+  const handleclickLinkedIn = () =>{
+    window.open("https://www.linkedin.com/in/cliftan", "_blank");
+  }
+  
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
       <section className="contact">
-        <h2>Contact Me</h2>
-        <form>
-          <label>Email</label>
-          <input type="email" placeholder="Your email" />
-          <label>Message</label>
-          <textarea placeholder="Your message"></textarea>
-          <button type="submit">Send</button>
-        </form>
+        <div className='container'>
+          <button onClick={handleclickGithub}>
+            <FontAwesomeIcon icon={faSquareGithub} size="10x" style={{color: "#74C0FC",}}/>
+          </button>
+          <button onClick={handleclickLinkedIn}>
+            <FontAwesomeIcon icon={faLinkedin} size="10x" style={{color: "#74C0FC",}} />
+          </button> 
+          <h1>Contact Me</h1>
+          <form>
+            <label>Email</label>
+            <input type="email" placeholder="Your email" />
+            <label>Message</label>
+            <textarea placeholder="Your message"></textarea>
+            <button type="submit">Send</button>
+          </form>
+          </div>
       </section>
     </motion.div>
   );
